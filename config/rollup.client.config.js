2eae822d545fc8ceac5d4998bd1fdb71a8434051
Owen Ayres <owen.ayres@gmail.com>
2018-02-01 10:51:13 +0000

    Adds a client config

---
+++ config/rollup.client.config.js
@@ -0,0 +1,32 @@
+import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import includePaths from 'rollup-plugin-includepaths'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+export default {
+  input: 'src/clientHoc.js',
+  output: {
+    name: 'cohere-fetch-data',
+    file: 'lib/fetchData.js',
+    format: 'umd'
+  },
+  plugins: [
+    peerDepsExternal(),
+    includePaths({
+      paths: ['./']
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      preferBuiltins: false,
+      browser: true
+    }),
+    commonjs(),
+    uglify()
+  ]
+}
