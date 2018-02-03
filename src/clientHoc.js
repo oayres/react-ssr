1ea41a18c90b61abbde75d26b4319c9a40c2cedf
Owen Ayres <owen.ayres@gmail.com>
2018-02-03 17:04:56 +0000

    Refactors HOC for client side fetchData

--- src/clientHoc.js
+++ src/clientHoc.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { withRouter } from 'react-router'
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
 // import DefaultLoadingSpinner from './components/LoadingSpinner'
 
 const cohere = DecoratedComponent => {
-  const fetchData = (params = {}) => {
+  const fetchAllData = (params = {}) => {
     return new Promise((resolve, reject) => {
       const calls = DecoratedComponent.fetchData(params)
       const keys = Object.keys(calls)
@@ -23,12 +24,12 @@ const cohere = DecoratedComponent => {
     })
   }
 
-  class _decoratedForServerRender extends DecoratedComponent {
-    constructor (props) {
-      super(props)
-      this.recallFetchData = false
-      this.state = {
-        fetched: false
+  class _decoratedForServerRender extends React.Component {
+    componentWillMount () {
+      if (!this.state || typeof this.state.fetched !== 'undefined') {
+        this.recallFetchData = false
+        this.loaderRequired = false
+        this.setState({ fetched: false })
       }
     }
 
@@ -42,6 +43,50 @@ const cohere = DecoratedComponent => {
       }
     }
 
+    shouldComponentUpdate (nextProps, nextState) {
+      return nextState.fetched
+    }
+
+    fetchData () {
+      if (this.recallFetchData) {
+        this.loaderRequired = true
+
+        fetchAllData(this.props.match.params)
+          .then(props => {
+            DecoratedComponent.defaultProps = {...DecoratedComponent.defaultProps, ...props}
+            this.setState({ fetched: true })
+          })
+          .catch(error => {
+            console.warn('Failed to fetch some props for fetchData. Rendering anyway...', error)
+            this.setState({ fetched: true })
+          })
+      }
+    }
+
+    checkIfAlreadyAssignedToDefaultProps () {
+      if (DecoratedComponent._ssrProps) {
+        DecoratedComponent._ssrProps.forEach(key => {
+          if (!DecoratedComponent.defaultProps || typeof DecoratedComponent.defaultProps[key] === 'undefined') {
+            this.recallFetchData = true
+            this.loaderRequired = true
+          }
+        })
+      }
+    }
+
+    extractFromWindow () {
+      if (typeof window !== 'undefined') {
+        const { _dataFromServerRender = {} } = window.__STATE
+        const props = _dataFromServerRender[DecoratedComponent.name]
+
+        if (props) {
+          DecoratedComponent.defaultProps = {...DecoratedComponent.defaultProps, ...props}
+          this.recallFetchData = false
+          this.loaderRequired = false
+        }
+      }
+    }
+
     render () {
       // const LoadingSpinner = this.props.loadingSpinner || DefaultLoadingSpinner
 
@@ -49,54 +94,37 @@ const cohere = DecoratedComponent => {
         /**
          * Stage 1: See if it's in the window state...
          */
-        if (typeof window !== 'undefined') {
-          const { _dataFromServerRender = {} } = window.__STATE
-          const props = _dataFromServerRender[DecoratedComponent.name]
-
-          if (props) {
-            // this.props = {...this.props, ...props}
-            DecoratedComponent.defaultProps = {...DecoratedComponent.defaultProps, ...props}
-            this.recallFetchData = false
-          }
-        }
+        this.extractFromWindow()
 
         /**
          * Stage 2: See if data is now, or previously was, in defaultProps
          */
-        if (DecoratedComponent.propsForServerRender) {
-          DecoratedComponent.propsForServerRender.forEach(key => {
-            if (!DecoratedComponent.defaultProps || typeof DecoratedComponent.defaultProps[key] === 'undefined') {
-              this.recallFetchData = true
-            }
-          })
-        }
+        this.checkIfAlreadyAssignedToDefaultProps()
 
         /**
          * Stage 3: Call to fetch the data as not found...
          */
-        if (this.recallFetchData) {
-          fetchData(this.props.match.params)
-            .then(props => {
-              DecoratedComponent.defaultProps = {...DecoratedComponent.defaultProps, ...props}
-              this.setState({ fetched: true })
-            })
-            .catch(error => {
-              console.warn('Failed to fetch some props for fetchData. Rendering anyway...', error)
-              this.setState({ fetched: true })
-            })
-
-          if (this.props.disableLoadingSpinner) {
-            return null
-          }
-
-          return <span>Loading...</span>
-          // return <LoadingSpinner />
-        }
+        this.fetchData()
+      } else if (typeof window === 'undefined') {
+        this.loaderRequired = false // on server...
       }
 
-      return <DecoratedComponent {...this.props} />
+      const showLoader = !this.props.disableLoadingSpinner && !this.state.fetched && this.loaderRequired
+
+      return (
+        <span>
+          {showLoader && <LoadingSpinner />}
+          <DecoratedComponent {...this.props} />
+        </span>
+      )
     }
   }
+
+  // may not be needed...
+  _decoratedForServerRender._ssrWaitsFor = DecoratedComponent._ssrWaitsFor
+  _decoratedForServerRender._ssrProps = DecoratedComponent._ssrProps
+  _decoratedForServerRender.fetchData = DecoratedComponent.fetchData
+  // end of may not be needed
 
   _decoratedForServerRender._displayName = DecoratedComponent.name
   return withRouter(_decoratedForServerRender)
