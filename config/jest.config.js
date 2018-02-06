334fdba0f5becec11d7b2942e5789945b771416f
Owen Ayres <owen.ayres@gmail.com>
2018-02-06 09:07:52 +0000

    Adds snapshots

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
 import Helmet from 'react-helmet'
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -9,4 +10,5 @@ global.React = React
 global.mount = Enzyme.mount
 global.shallow = Enzyme.shallow
 global.render = Enzyme.render
+global.snapshot = toJson
 Helmet.canUseDOM = false
