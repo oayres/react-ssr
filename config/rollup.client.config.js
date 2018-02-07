54d779db6781617313c18c3d517ac15f1d5e50a2
Owen Ayres <owen.ayres@gmail.com>
2018-02-07 18:28:16 +0000

    Changes entry point to HOC

--- config/rollup.client.config.js
+++ config/rollup.client.config.js
@@ -6,7 +6,7 @@ import includePaths from 'rollup-plugin-includepaths'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 export default {
-  input: 'src/clientHoc.js',
+  input: 'src/decorators/ssrFetchData.js',
   output: {
     name: 'cohere-fetch-data',
     file: 'lib/fetchData.js',
