c76bb1b1f89059d6e355f7ed57adc138805c9650
Owen Ayres <owen.ayres@gmail.com>
2018-02-20 20:03:41 +0000

    Adds meaningful tests to fetchData logic

--- src/helpers/fetchData/fetchData.test.js
+++ src/helpers/fetchData/fetchData.test.js
@@ -1,8 +1,70 @@
 import fetchData from './fetchData'
 
+const fakeCall = (fakeThingToResolve) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fakeThingToResolve)
+    }, 10)
+  })
+}
+
 test('will directly return promises if component does not have _ssrWaitsFor or fetchData', () => {
   const component = {}
   const result = fetchData(component, {}, [])
   expect(Array.isArray(result)).toBeTruthy()
   expect(result).toEqual([])
+})
+
+test('will add a new promise in if the component has a fetchData', () => {
+  const component = { fetchData: jest.fn() }
+  const result = fetchData(component, {}, [])
+  expect(result.length).toEqual(1)
+})
+
+test('it populates the promises array with 2 promises if there is one in _ssrWaitsFor', () => {
+  const component = {
+    fetchData: jest.fn(),
+    _ssrWaitsFor: [{
+      fetchData: jest.fn()
+    }]
+  }
+
+  const result = fetchData(component, {}, [])
+
+  expect(result.length).toEqual(2)
+})
+
+test('it rejects with the expected error when fetchData is not a function', async () => {
+  const component = { fetchData: 'invalid-thing' }
+  const result = fetchData(component, {}, [])
+  await expect(result[0]).rejects.toEqual(
+    new Error('Fetch data not defined or not a function.')
+  )
+})
+
+test('it resolves with the expected props from a single fetchData', async () => {
+  const data = { test: 'Game of Thrones!' }
+  const component = {
+    fetchData: function () {
+      return fakeCall(data)
+    }
+  }
+
+  const result = fetchData(component, {}, [])
+  await expect(result[0]).resolves.toEqual({ defaultProps: data, fetchData: component.fetchData })
+})
+
+test('it resolves multiple keys when returning an object of several promises', async () => {
+  const data = { one: 'Game of Thrones!', two: 'Westworld' }
+  const component = {
+    fetchData: function () {
+      return {
+        one: fakeCall(data.one),
+        two: fakeCall(data.two)
+      }
+    }
+  }
+
+  const result = fetchData(component, {}, [])
+  await expect(result[0]).resolves.toEqual({ defaultProps: data, fetchData: component.fetchData })
 })
